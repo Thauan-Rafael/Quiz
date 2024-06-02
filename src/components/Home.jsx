99af9ad52380f8b53ecb2b41572fb22e11574c52
Thauan Rafael <thauanrafaeltrabalho@gmail.com>
2024-06-01 21:38:21 -0300

    Second day of project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 function Home(props){
     return(
         <>
-            <h2 id="homeTitle">Welcome to the Game !</h2>
-            <li>Infinite questions</li>
-            <li>Random generated numbers</li>
-            <li>10 seconds to guess</li>
-            <li>Earn 1 point per correct answer</li>
-            <button id='playButton' onClick={props.openQuests}>Start Quiz</button>
+            <div id="homeContent">
+                <h2 id="homeTitle">Welcome to the Game !</h2>
+                <li>Infinite questions</li>
+                <li>Random generated numbers</li>
+                <li>10 seconds to guess</li>
+                <li>Earn 1 point per correct answer</li>
+                <button id='playButton' onClick={props.openQuests}>Start Quiz</button>
+            </div>
         </>
     )
 }
--- src/components/Quest.jsx
+++ src/components/Quest.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 function Quest(){
-    let questNumber = 1;
-    let timer = 10;
+    let numbers = []
+    let operators = []
+    const [questNumber,setQuestNumber] = React.useState(1);
+    const [timer, setTimer] = React.useState(10)
+    const [quest, setQuest] = React.useState(generateQuest())
+    const formatTime = (time) => {
+        return time < 10 ? `0${time}` : time
+    };
+    React.useEffect(() => {
+        if(timer > 0){
+            const countdown = setTimeout(() => {setTimer(timer - 1)}, 1000);
+            return () => clearTimeout(countdown);
+        }
+    },[timer])
+    function generateQuest(){
+        return `${Math.floor(Math.random()*100)} + ${Math.floor(Math.random()*100)}`
+    }
+    generateQuest()
     return(
         <>
             <div id='questPage'>
                 <header id='questHeader'>
+                    <h3 id='points'>Points: {questNumber-1}</h3>
                     <h2>Quest {questNumber}</h2>
-                    <h3>{timer}</h3>
+                    <h3 id='questTimer'>{formatTime(timer)}</h3>
                 </header>
-                <div id='mathQuest'>5 x 10</div>
-                <input id='userInput' placeholder='Answer'></input>
+                <div id='mathQuest'>{quest}</div>
+                <input id='userInput' placeholder='Answer' type='number'></input>
                 <button id='confirmButton'>Confirm</button>
             </div>
         </>
